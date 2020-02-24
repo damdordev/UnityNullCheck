8fdde88bdb6063984ba50f2ed74d08979759993a
Damian Doroba <damdor.dev@gmail.com>
2020-02-24 20:38:51 +0100

    property drawer

---
+++ Assets/Code/Damdor/Utils/Editor/WrapperPropertyDrawer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Damdor.Utils.Editor
+{
+    [CustomPropertyDrawer(typeof(Wrapper), true)]
+    public class WrapperPropertyDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(GetValueProperty(property));
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.PropertyField(position, GetValueProperty(property), GUIContent.none);
+            
+        }
+        
+        private SerializedProperty GetValueProperty(SerializedProperty property) => property.FindPropertyRelative("value");
+        
+    }
+}
